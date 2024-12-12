6fb617c7aed5c582dcb693373f543ee4c1504991
alan <alanzapadka@yahoo.com>
2024-12-12 20:41:00 +0000

    upd commit

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Button } from './Button'
 import { Link } from 'react-router-dom'
-import './Footer.css'
+import './footer.css'
 function Footer() {
   return (
     <div className='footer-container'>
